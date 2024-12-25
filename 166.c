abb00d500d066b4ba6524dc1da89999aa727e161
wyiju <a0968299512@gmail.com>
2024-12-25 21:54:22 +0800

    f91

---
+++ 166.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int f91(int n) {
+    if (n >= 101)
+        return n - 10;
+    else
+        return f91(f91(n + 11));
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    printf("f91(%d) = %d\n", n, f91(n));
+    return 0;
+
+}
