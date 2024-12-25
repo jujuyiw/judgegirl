6fdda4db1343050209919532c1c513d55facefa1
wyiju <a0968299512@gmail.com>
2024-12-26 01:51:35 +0800

    floodfill

--- .142.c
+++ .142.c
@@ -2,34 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void floodFill(int** image, int imageSize, int sr, int sc, int newColor,int origin) {
-    
-    if (sr+1  < imageSize)
-        if (image[sr+1][sc] ==origin ) {
-            image[sr+1][sc] = -1;
-            floodFill(image, imageSize, sr+1 , sc, newColor,origin);
-        }
-    if (sr - 1 >= 0)
-        if (image[sr - 1][sc] == origin) {
-            image[sr - 1][sc] = -1;
-            floodFill(image, imageSize, sr -1, sc, newColor,origin);
-        }
-    if (sc +1 < imageSize)
-        if (image[sr][sc+1] == origin) {
-            image[sr][sc+1] = -1;
-            floodFill(image, imageSize, sr , sc+1, newColor,origin);
-        }
-    if (sc - 1 >= 0)
-        if (image[sr][sc - 1] == origin) {
-            image[sr][sc - 1] = -1;
-            floodFill(image, imageSize, sr, sc-1, newColor,origin);
-        }
-    for (int i = 0;i < imageSize;i++)
-        for (int j = 0;j < imageSize;j++)
-            if (image[i][j] == -1)
-                image[i][j] = newColor;
-    
+void check(int** image, int imageSize, int sr, int sc, int newColor,int origin) {
+    if (sr >= imageSize || sr < 0 || sc >= imageSize || sc < 0)
+        return;
+    if (image[sr][sc] != origin)
+        return;
+    image[sr][sc] = newColor;
+
+    check(image, imageSize, sr + 1, sc, newColor, origin);
+    check(image, imageSize, sr - 1, sc, newColor, origin);
+    check(image, imageSize, sr, sc + 1, newColor, origin);
+    check(image, imageSize, sr, sc - 1, newColor, origin);
     return;
+}
+
+void floodFill(int** image, int imageSize, int sr, int sc, int newColor) {
+    int origin = image[sr][sc];
+    if (origin == newColor)
+        return;
+    check(image, imageSize, sr, sc, newColor, origin);
 }
 
 int main() {
@@ -51,9 +42,8 @@ int main() {
     scanf("%d", &sr);
     scanf("%d", &sc);
     scanf("%d", &newColor);
-    int origin = p[sr][sc];
-    p[sr][sc] = -1;
-    floodFill(p, n, sr, sc, newColor,origin);
+    int targent = p[sr][sc];
+    floodFill(p, n, sr, sc, newColor);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
